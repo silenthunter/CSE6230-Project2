e01376096ab3265824e269769f45f29370c43b41
silenthunter <silenthunter@gmail.com>
2011-11-18 23:30:25 -0500

    Makes the grid launch size variable based on the image dimensions and cuda block thread count

--- CUDA/EdgeDetection.cu
+++ CUDA/EdgeDetection.cu
@@ -3,6 +3,11 @@
 #include<stdio.h>
 #include "bmpLoader.cu"
 
+//Program constants
+const int cuda_threadX = 32;
+const int cuda_threadY = 16;
+const int gaussSize = 3;
+
 __device__ float e = 2.71828f;
 __device__ float PI = 3.14f;
 __device__ float dev = 1.4f;
@@ -19,8 +24,6 @@ __device__ float GaussianBlur(int x, int y)
 
 __global__ void MakeBW(float* d_image, float* d_bw, int d_width)
 {
-	width = d_width;
-	image = d_bw;
 	//TODO: Make local shared array to maximize speed
 	int dest = (threadIdx.x + threadIdx.y * blockDim.x) + 
 		(blockIdx.x + blockIdx.y * gridDim.x) * (blockDim.x * blockDim.y);
@@ -30,6 +33,13 @@ __global__ void MakeBW(float* d_image, float* d_bw, int d_width)
 	float val = d_image[idx] *.3 + d_image[idx + 1] * .59 + d_image[idx + 2] * .11;
 
 	d_bw[dest] = val;
+	
+	//Have thread 0 assign the global variables
+	if(dest == 0)
+	{
+		image = d_bw;
+		width = d_width;
+	}
 }
 
 __global__ void ComputeGuassian(float* d_gaussMat, int width)
@@ -46,7 +56,6 @@ __global__ void GaussianBlur(int width)
 int main(int argc, char* argv[])
 {
 	//Have the card compute the matrix for the gaussion blur
-	int gaussSize = 3;
 	float* d_gaussMat;
 	float* h_gaussMat = (float*)malloc(sizeof(float) * gaussSize * gaussSize);
 	cudaMalloc((void**)&d_gaussMat, sizeof(float) * gaussSize * gaussSize);
@@ -77,14 +86,14 @@ int main(int argc, char* argv[])
 	cudaMalloc((void**)&d_bw, sizeof(float) * imageSize / 3);
 	cudaMemcpy(d_image, h_image, sizeof(float) * imageSize, cudaMemcpyHostToDevice);
 	
-	blockSize = dim3(32, 16);
-	gridSize = dim3(32, 48);
+	//Convert the image into black and white
+	blockSize = dim3(cuda_threadX, cuda_threadY);
+	gridSize = dim3(bmpInfo.biWidth / cuda_threadX, bmpInfo.biHeight / cuda_threadY);
 	MakeBW<<<gridSize, blockSize>>>(d_image, d_bw, bmpInfo.biWidth);
 	h_bw = (float*)malloc(sizeof(float) * imageSize);
 	cudaThreadSynchronize();
 	cudaMemcpy(h_bw, d_bw, sizeof(float) * imageSize / 3, cudaMemcpyDeviceToHost);
 	
-	printf("%f\n", h_bw[0]);
 	for(int i = 0; i < imageSize; i++)
 	{
 		cImage[i] = (unsigned char)(h_bw[i / 3] * 256);
